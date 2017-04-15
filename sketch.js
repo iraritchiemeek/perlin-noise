babfd119c85abd2a849bbc92d3c0577c10becbd9
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-04-15 15:13:58 +1200

    ellipse moving along perlin noise x and y axis with different xoffsets

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
-var xoff = 0
+var xoff1 = 0
+var xoff2 = 10000
 
 function setup() {
 	createCanvas(400, 400)
@@ -7,9 +8,11 @@ function setup() {
 function draw() {
   background(51)
   // var x = random(width)
-  var x = map(noise(xoff), 0, 1, 0, width)
+  var x = map(noise(xoff1), 0, 1, 0, width)
+  var y = map(noise(xoff2), 0, 1, 0, height)
 
-  xoff += 0.01
+  xoff1 += 0.02
+  xoff2 += 0.02
 
-  ellipse(x, 200, 24, 24)
+  ellipse(x, y, 24, 24)
 }
