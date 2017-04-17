c639bc61239ce5f26c62415a6cb39a5011bd4604
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-04-18 08:27:15 +1200

    lines with perlin noise angles and zoff

--- sketch.js
+++ sketch.js
@@ -1,26 +1,39 @@
-var inc = 0.01
-var scl = 20
+var inc = 0.1
+var scl = 10
 var cols, rows
+
+var zoff = 0
+
+var fr
 
 function setup() {
 	createCanvas(200, 200)
 	pixelDensity(1)
 	cols = floor(width/scl)
 	rows = floor(height/scl)
+	fr = createP('')
 }
 
 function draw() {
+	background(255)
 	var yoff = 0
 	for (var y = 0; y < rows; y++) {
 		var xoff = 0
 		for (var x = 0; x < cols; x++) {
 			var index = (x + y * width) * 4
-			var r = noise(xoff, yoff) * 255
+			var angle = noise(xoff, yoff, zoff) * TWO_PI
+			var v = p5.Vector.fromAngle(angle)
 			xoff += inc
-
-			fill(random(256))
-			rect(x * scl, y * scl, scl, scl)
+			stroke(0)
+			push()
+			translate(x * scl, y * scl)
+			rotate(v.heading())
+			line(0, 0, scl, 0)
+			pop()
 		}
 		yoff += inc
+		zoff += 0.001
 	}
+
+	fr.html(floor(frameRate()))
 }
